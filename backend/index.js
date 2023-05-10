76654ff6c5d821a7b3f9eb76d8b025f2a0b0f366
John Lenz <john.lenz27@gmail.com>
2023-05-10 11:47:47 -0700

    have a connected database

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config()
+
+mongoose
+.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => console.log(err));
+
+
+app.listen(8800,()=>{
+    console.log("Server is running on port 8800");
+})
