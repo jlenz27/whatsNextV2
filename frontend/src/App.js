8f90b0058fc536ebdd5971c3e6f5a357bc2df6a5
John Lenz <john.lenz27@gmail.com>
2023-05-12 17:21:24 -0700

    working on connecting the frontend to the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Marker } from 'mapbox-gl';
+import axios from 'axios';
 
 function App() {
+  const [pins, setPins] = useState([])
+
   const mapContainerRef = useRef(null);
 
   useEffect(() => {
@@ -12,10 +15,40 @@ function App() {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [32.7157, 117.1611],
-      zoom: 8
+      center: [-117.1611, 32.7157],
+      zoom: 12
+
     });
-  }, []);
+    const marker = new mapboxgl.Marker({
+      color: "Blue",
+      draggable: false
+    }).setLngLat([-117.1611, 32.7157])
+      .addTo(map);
+
+
+      pins.map(p => (
+        new mapboxgl.Popup({ closeOnClick: false })
+          .setLngLat([p.long, p.lat])
+          .setHTML(p.title + p.desc + p.username)
+          .addTo(map)
+      ))
+
+  }, [pins]);
+  
+  
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, [])
 
   return (
     <div>
